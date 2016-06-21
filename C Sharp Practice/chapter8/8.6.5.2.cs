e35ff59f13b97174579315d35cf7633d8d4eca19
wusong4988398 <1801273147@qq.com>
2016-06-21 21:40:10 +0800

    222

---
+++ C Sharp Practice/chapter8/8.6.5.2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace chapter8
+{
+     [TestClass]
+    public class Class1
+    {
+         /// <summary>
+         /// 测试泛型的约束
+         /// </summary>
+         [TestMethod]
+         public void Test01()
+         {
+             BaseClass b=new DerivedClass();
+             b.MyFunc<int>(100);
+             b.MyFunc<char>('A');
+            // b.MyFunc<string>("hello");//出现编译错误
+             //原因是string类型没有公共的无参构造函数，可见在子类中已经默认继承来父类的约束。
+         }
+         /// <summary>
+         /// 测试泛型方法的重载
+         /// </summary>
+         public void Test02()
+         {
+             BaseClass b = new DerivedClass();
+             b.MyFunc<int>(100);
+             b.MyFunc<char>('A');
+             b.MyFunc("100");
+             // b.MyFunc<string>("hello");//出现编译错误
+             
+         }
+    }
+
+
+    class BaseClass
+    {
+        public virtual void MyFunc<T>(T t) where T:new()
+        {
+            Console.WriteLine("{0} in Base",t.ToString());
+        }
+
+        public void MyFunc(string t)
+        {
+            Console.WriteLine(t);
+        }
+    }
+    class DerivedClass:BaseClass
+    {
+        public override void MyFunc<T>(T t)
+        {
+            Console.WriteLine("{0} in Derived",t.ToString());
+            
+        }
+    }
+}
---
+++ C Sharp Practice/chapter8/class_8_7_2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace chapter8
+{
+    [TestClass]
+  public  class Class872
+    {
+        /// <summary>
+        /// 测试浅拷贝
+        /// </summary>
+         [TestMethod]
+        public void Test02()
+        {
+            Student s1=new Student("wusong",22);
+            //执行浅拷贝
+            Student s2 = s1;
+            s2.Age = 27;
+            s1.ShowInfo();
+        }
+                /// <summary>
+        /// 测试深拷贝
+        /// </summary>
+         [TestMethod]
+        public void Test03()
+        {
+            Int32 i=100;
+            Int32 j = i;
+            j = 200;
+            Console.WriteLine(i);
+        }
+    }
+    }
+
+
+
+    class Student
+    {
+        public string Name;
+        public Int32 Age;
+
+        public Student(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("{0} age is {1}",Name,Age);
+        }
+    }
+}
